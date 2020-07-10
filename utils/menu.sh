62b4ab4e65bb579990a98dd86b6269f7dfc2765e
Clive Walkden <clive@sozodesign.co.uk>
2020-07-10 11:02:28 +0100

    Added menu option to allow full database to be syncronized.

---
+++ utils/menu.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+die()
+{
+	local _ret=$2
+	test -n "$_ret" || _ret=1
+	test "$_PRINT_HELP" = yes && dbsynchelp >&2
+	echo "$1" >&2
+	exit ${_ret}
+}
+
+
+# Function that evaluates whether a value passed to it begins by a character
+# that is a short option of an argument the script knows about.
+# This is required in order to support getopts-like short options grouping.
+begins_with_short_option()
+{
+	local first_option all_short_options='fh'
+	first_option="${1:0:1}"
+	test "$all_short_options" = "${all_short_options/$first_option/}" && return 1 || return 0
+}
+
+# THE DEFAULTS INITIALIZATION - OPTIONALS
+_arg_full="off"
+
+dbsynchelp() {
+  cat <<-HEREDOC
+  Magento 2 DB Sync
+
+  Syncronize a database from production to staging, testing or development environments.
+
+  Usage: db-sync.sh [-f|--(no-)full] [-h|--help]
+  Options:
+    -f, --full, --no-full: full database dump (off by default)
+    -h, --help: Prints help
+HEREDOC
+}
+
+# The parsing of the command-line
+parse_commandline()
+{
+	while test $# -gt 0
+	do
+		_key="$1"
+		case "$_key" in
+			# The full argurment doesn't accept a value,
+			# we expect the --full or -f, so we watch for them.
+			-f|--no-full|--full)
+				_arg_full="on"
+				test "${1:0:5}" = "--no-" && _arg_full="off"
+				;;
+			# We support getopts-style short arguments clustering,
+			# so as -f doesn't accept value, other short options may be appended to it, so we watch for -f*.
+			# After stripping the leading -f from the argument, we have to make sure
+			# that the first character that follows coresponds to a short option.
+			-f*)
+				_arg_full="on"
+				_next="${_key##-f}"
+				if test -n "$_next" -a "$_next" != "$_key"
+				then
+					{ begins_with_short_option "$_next" && shift && set -- "-f" "-${_next}" "$@"; } || die "The short option '$_key' can't be decomposed to ${_key:0:2} and -${_key:2}, because ${_key:0:2} doesn't accept value and '-${_key:2:1}' doesn't correspond to a short option."
+				fi
+				;;
+			# See the comment of option '--full' to see what's going on here - principle is the same.
+			-h|--help)
+				dbsynchelp
+				exit 0
+				;;
+			# See the comment of option '-f' to see what's going on here - principle is the same.
+			-h*)
+				dbsynchelp
+				exit 0
+				;;
+			*)
+				_PRINT_HELP=yes die "FATAL ERROR: Got an unexpected argument '$1'" 1
+				;;
+		esac
+		shift
+	done
+}
+
+parse_commandline "$@"
--- utils/remote_backup.sh
+++ utils/remote_backup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # Use the settings to check your connection
-echo -e "\n${txt_white}${bg_black}Backing up remote database(s).${txt_end}\n"
+echo -e "\n${txt_white}${bg_black}Backing up remote database(s).   ${txt_end}\n"
+if [ $_arg_full == 'on' ]; then
+echo -e "\n${txt_white}${bg_black}  Full customer and order backup.${txt_end}\n"
+fi
 
 ssh -p "${remote_port}" "${remote_username}@${remote_host}" <<ENDSSH
 mkdir -p $remote_backup_dir
@@ -17,7 +20,12 @@ REMOTE_DB_WP_USER=$(n98-magerun config:env:show db.connection.wordpress.username
 REMOTE_DB_WP_PASS=$(n98-magerun config:env:show db.connection.wordpress.password)
 REMOTE_DB_WP_DBASE=$(n98-magerun config:env:show db.connection.wordpress.dbname)
 
+if [ $_arg_full == 'on' ]; then
+n98-magerun db:dump --compression="gzip" --strip="@log @sessions" --force $remote_backup_dir/latest-m2.sql.gz
+else
 n98-magerun db:dump --compression="gzip" --strip="@log @sessions @trade @sales" --force $remote_backup_dir/latest-m2.sql.gz
+fi
+
 if [ ! -z $REMOTE_DB_WP_HOST ]; then
 mysqldump -h$REMOTE_DB_WP_HOST -u$REMOTE_DB_WP_USER -p$REMOTE_DB_WP_PASS $REMOTE_DB_WP_DBASE>$remote_backup_dir/latest-wp.sql
 gzip -f $remote_backup_dir/latest-wp.sql
