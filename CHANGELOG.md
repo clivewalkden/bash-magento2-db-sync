bde83a2d1d0cd603ed5647c25ce8a1551c947356
Clive Walkden <clive@sozodesign.co.uk>
2022-10-11 12:38:12 +0100

    Added the --force flag to imports

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+- Added the `--force` flag for db imports
+
 
 ## [1.8.0] - 2022-01-06
 ### Added
--- utils/local_import.sh
+++ utils/local_import.sh
@@ -5,11 +5,14 @@ echo
 echo -e "${txt_blue}  Importing into local database(s).${txt_end}"
 echo
 
-n98-magerun db:import --drop-tables $local_backup_dir/latest-m2.sql
+n98-magerun db:import --force --drop-tables $local_backup_dir/latest-m2.sql
 
 if [ $_arg_wordpress == 'on' ]; then
     cd wp
     sed -i '1s/^/SET SQL_MODE='\''ALLOW_INVALID_DATES'\'';\n\n/' $local_backup_dir/latest-wp.sql
     
     wp db import $local_backup_dir/latest-wp.sql
+
+    wp search-replace "www.$old_domain" "$new_domain" --skip-columns=guid
+    wp search-replace "$old_domain" "$new_domain" --skip-columns=guid
 fi
